eb03e3c0d75d8c3bf6af10ce030eb62831a29034
Chris Herrmann <chris.herrmann@domesticandgeneral.com>
2023-03-13 11:52:06 +0100

    add header and logo assets

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
 
+import Header from "@/components/molecules/header/header";
+import dgLogo from "@/assets/dgLogo.svg";
+import dgLogoFull from "@/assets/dgLogoFull.svg";
+
 interface ILayout {
   children: React.ReactNode;
 }
@@ -10,7 +14,31 @@ const LayoutMain = ({ children }: ILayout): JSX.Element => {
       <Head>
         <title>DG Click Welcome</title>
       </Head>
-      {/*  */}
+      {/* Page header */}
+      <header>
+        <Header
+          className=""
+          primaryLogoBar={{
+            image: {
+              src: dgLogoFull,
+              alt: "hughes-logo",
+              className: "",
+              priority: true,
+            },
+            bgClassName: "px-6 md:px-12 bg-white",
+            className: "flex items-center justify-start py-6",
+          }}
+          secondaryLogoBar={{
+            image: {
+              src: dgLogo,
+              alt: "dg-logo",
+              className: "",
+            },
+            bgClassName: "bg-[#F5F5F5] px-6 md:px-12",
+            className: "flex items-center justify-end py-2",
+          }}
+        />
+      </header>
 
       {/* Main grid container */}
       <main>
--- src/components/molecules/header/header.tsx
+++ src/components/molecules/header/header.tsx
@@ -1,30 +1,30 @@
-import LogoBar, { ILogoBar } from '@/components/atoms/logoBar/logoBar'
+import LogoBar, { ILogoBar } from "@/components/atoms/logoBar/logoBar";
 
 type IHeader = {
-    className: string
-    primaryLogoBar: ILogoBar
-    secondaryLogoBar: ILogoBar
-}
+  className: string;
+  primaryLogoBar: ILogoBar;
+  secondaryLogoBar: ILogoBar;
+};
 
 const Header = ({
-    className,
-    primaryLogoBar,
-    secondaryLogoBar,
+  className,
+  primaryLogoBar,
+  secondaryLogoBar,
 }: IHeader): JSX.Element => {
-    return (
-        <div className={className}>
-            <LogoBar
-                bgClassName={primaryLogoBar.bgClassName}
-                className={primaryLogoBar.className}
-                image={primaryLogoBar.image}
-            />
-            <LogoBar
-                bgClassName={secondaryLogoBar.bgClassName}
-                className={secondaryLogoBar.className}
-                image={secondaryLogoBar.image}
-            />
-        </div>
-    )
-}
+  return (
+    <div className={className}>
+      <LogoBar
+        bgClassName={primaryLogoBar.bgClassName}
+        className={primaryLogoBar.className}
+        image={primaryLogoBar.image}
+      />
+      <LogoBar
+        bgClassName={secondaryLogoBar.bgClassName}
+        className={secondaryLogoBar.className}
+        image={secondaryLogoBar.image}
+      />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
